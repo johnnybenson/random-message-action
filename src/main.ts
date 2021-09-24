72cfdd39e92b15fe45f4922ace824613ed23b71a
Johnny Benson <johnnybenson@users.noreply.github.com>
2021-09-24 17:20:18 -0400

    Initial commit, tell me something good bb

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,67 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const DEFAULT_FILE_PATH = 'random.json';
+const MESSAGE = 'message';
+
+export function rand<T>(arr: T[]): T {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+async function getData(
+  filePath = DEFAULT_FILE_PATH,
+  ref: string,
+  octokit: ReturnType<typeof github.getOctokit>,
+): Promise<string[]> {
+  const data = (
+    await octokit.rest.repos.getContent({
+      ...github.context.repo,
+      path: filePath,
+      ref,
+    })
+  ).data as { content: string };
+
+  const jsonData = data.content;
+
+  if (!jsonData) {
+    throw new Error(`Could not find ${filePath} for commit ${ref}`);
+  }
+
+  return JSON.parse(Buffer.from(jsonData, 'base64').toString());
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    /**
+     * Get Workflow Input
+     */
+    const GITHUB_TOKEN =
+      core.getInput('GITHUB_TOKEN') || process.env.GITHUB_TOKEN;
+    const JSON_FILE_PATH =
+      core.getInput('JSON_FILE_PATH') || process.env.JSON_FILE_PATH;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (typeof GITHUB_TOKEN !== 'string') {
+      throw new Error(
+        'Invalid GITHUB_TOKEN: did you forget to set it in your action config?',
+      );
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    /**
+     * Read Data from JSON
+     */
+    const data = await getData(
+      JSON_FILE_PATH,
+      github.context.sha,
+      github.getOctokit(GITHUB_TOKEN),
+    );
+
+    /**
+     * Output random message
+     */
+    core.setOutput(MESSAGE, rand(data));
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
